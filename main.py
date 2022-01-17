366c3fd4c279f62254d4283caab34a0192f2a0af
Alexander St. John <avstjohn@icloud.com>
2022-01-16 21:35:15 -0600

    use flask and add inputs to html

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+#from stat import IO_REPARSE_TAG_APPEXECLINK
 from flask import Flask, render_template
+from prototype_backend import *
 
 app = Flask(__name__)
-@app.route("/")
 
+@app.route("/")
 def index(): 
     my_string = "hell world"
-    album_uri = 'spotify:album:0f4TeyGJMm9iLjD38er2lO'.split(':')[-1]
+
+    # Call to prototype_backend.py to calculate reutrn from search input
+    track_uri = get_results()
+
+    #album_uri = 'spotify:album:0f4TeyGJMm9iLjD38er2lO'.split(':')[-1]
     # Hook up to prototype_backend.py so that track_uri is fetched on the backend
-    track_uri = 'spotify:track:1elJ0Y0i1ZxzpbuLgK3YTO'.split(':')[-1]
+    #track_uri = 'spotify:track:1elJ0Y0i1ZxzpbuLgK3YTO'.split(':')[-1]
+    
     return render_template("index.html", body=my_string, uri=track_uri)
 
 if __name__ == "__main__":
--- prototype_backend.py
+++ prototype_backend.py
@@ -5,27 +5,30 @@ from spotipy.oauth2 import SpotifyOAuth
 scope = "user-modify-playback-state"
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-if len(sys.argv) > 1:
-    name = ' '.join(sys.argv[1:])
-else:
-    name = 'varese'
+def get_results():
+    if len(sys.argv) > 1:
+        name = ' '.join(sys.argv[1:])
+    else:
+        name = 'varese'
 
-total = spotify.search(q='track:' + name, type='track')['tracks']['total']
-# Workaround when total returns greater than 1000 and the offset in the next query is out of bounds
-# Bug? Server side processing to trim and choose "page" of results?
-if (total > 1000):
-    total = int(total / 10)
+    total = spotify.search(q='track:' + name, type='track')['tracks']['total']
+    # Workaround when total returns greater than 1000 and the offset in the next query is out of bounds
+    # Bug? Server side processing to trim and choose "page" of results?
+    if (total > 1000):
+        total = int(total / 10)
 
-if (total > 0):
-    results = spotify.search(q='track:' + name, type='track', limit=1, offset=total-1)
-    track_uri = results['tracks']['items'][0]['uri']
-    album_uri = results['tracks']['items'][0]['album']['uri']
-    artist_uri = results['tracks']['items'][0]['artists'][0]['uri']
+    if (total > 0):
+        results = spotify.search(q='track:' + name, type='track', limit=1, offset=total-1)
+        track_uri = results['tracks']['items'][0]['uri']
+        album_uri = results['tracks']['items'][0]['album']['uri']
+        artist_uri = results['tracks']['items'][0]['artists'][0]['uri']
 
-    # To do: use API to get and choose device_id
-    # Will be modified to use the embedded player to play track once we have a frontend
-    # MacBook
-    device_id = '83e760a679b4a01af5c48b6dc7a9dfb4d7c08ee4'
-    # PhoneyBologna
-    #device_id = '0b0c9d95bbeb8fcb45d96561a7080e70b4cf164d'
-    spotify.start_playback(device_id=device_id, uris=[track_uri])
+        # To do: use API to get and choose device_id
+        # Will be modified to use the embedded player to play track once we have a frontend
+        # MacBook
+        device_id = '83e760a679b4a01af5c48b6dc7a9dfb4d7c08ee4'
+        # PhoneyBologna
+        #device_id = '0b0c9d95bbeb8fcb45d96561a7080e70b4cf164d'
+        spotify.start_playback(device_id=device_id, uris=[track_uri])
+    track_uri = track_uri.split(':')[-1]
+    return track_uri
